5f548e9ea2bb00a2b1a778c6a2df74dbae85c678
Abdullah Kady <abdullahkady@gmail.com>
2018-05-03 16:22:48 +0200

    Added the rest of the proccessor - YARAB :D

--- cpu.v
+++ cpu.v
@@ -1,4 +1,5 @@
-module CPU(clk)
+module CPU(clk);
+  input clk;
   reg [31:0] PC; 
   initial begin
     PC = 0;
@@ -10,9 +11,9 @@ module CPU(clk)
   //CONTROL SIGNALS
   wire regDestFLAG;
   wire branchFLAG;
-  wire memReadFLAG;
+  wire[1:0] memReadFLAG;
   wire memToRegFLAG;
-  wire ALU_SELECTION;
+  wire[2:0] ALU_SELECTION;
   wire memWriteFLAG;
   wire aluSrcFLAG;
   wire regWriteFLAG;
@@ -23,8 +24,17 @@ module CPU(clk)
   wire[31:0] registerFileReadData_1;
   wire[31:0] registerFileReadData_2;  
   wire[31:0] registerFileWriteData;  
-  
   //REGISTER FILE
+
+  //ALU
+  wire[31:0] ALU_result;
+  wire zeroFLAG;
+  wire[31:0] aluInputData_2;
+  //ALU
+
+  //DATA MEMORY
+  wire[31:0] dataMemoryOut;
+  //DATA MEMORY
 
   always @(posedge clk) begin
     PC <= newPC;
@@ -35,14 +45,16 @@ module CPU(clk)
   assign PC_PLUS_4 = PC + 4;
   assign PC_ADDER = PC_PLUS_4 + (immediateValueExtended<<2);
 
-  assign branchMUX = branchFLAG & ZF;
+  assign branchMUX = branchFLAG & zeroFLAG;
 
   //PC select mux 
   assign newPC = (branchMUX) ? PC_ADDER : PC_PLUS_4;
-  // Write register MUX
+  //Write register MUX
   assign writeRegisterAddress = (regDestFLAG) ? currentInstruction[15:11] : currentInstruction[20:16];
-
-
+  //ALU 2nd input MUX
+  assign aluInputData_2 = (aluSrcFLAG) ? immediateValueExtended : registerFileReadData_2;
+  //Write register file MUX
+  assign registerFileWriteData = (memToRegFLAG) ? dataMemoryOut : ALU_result;
 
   IntstructionMemory instMemory(
     PC,
@@ -73,8 +85,25 @@ module CPU(clk)
     clk
   );
 
-  
+  ALU alu(
+    ALU_result,
+    zeroFLAG,
+    registerFileReadData_1,
+    aluInputData_2,
+    ALU_SELECTION,
+    currentInstruction[10:6]
+  );
 
+  DataMemory dataMemory(
+    ALU_result,
+    registerFileReadData_2,
+    memWriteFLAG,
+    memReadFLAG,
+    dataMemoryOut,
+    clk
+  );
+
+  
 endmodule
 
 
@@ -238,6 +267,99 @@ module RegisterFile (readAddress_1, readAddress_2, writeAddress, outputData_1, o
     end
 	end
 
+endmodule
+
+module	ALU	(OUT,	ZeroFlag,	input1_unsigned,	input2_unsigned,ALU_SELECTION, SHIFT_AMOUNT);
+				
+	input	[31:0]	input1_unsigned,	input2_unsigned;
+	input	[2:0]	ALU_SELECTION;
+	input [4:0] SHIFT_AMOUNT;
+	output	reg	[31:0]	OUT;
+	output	ZeroFlag;
+	integer input1_signed,input2_signed;
+				
+
+	// BEQ 
+	assign ZeroFlag = (input1_unsigned == input2_unsigned);
+
+	// ALL OTHER CASES
+	always	@	(*)
+	begin
+		//SIGNED VALUES 
+		input1_signed = input1_unsigned;
+		input2_signed = input2_unsigned;
+
+		case	(ALU_SELECTION)
+			0	:	OUT	=	input1_signed	+	input2_signed; //ADD
+			1	:	OUT	=	input1_signed	- input2_signed; //SUB  
+			2	:	OUT	=	input1_unsigned	<< SHIFT_AMOUNT; //SLL 
+			3	:	OUT	=	input1_unsigned >> SHIFT_AMOUNT; //SRL 
+			4	:	OUT	=	input1_unsigned	&	input2_unsigned; //AND
+			5	:	OUT	=	input1_unsigned	|	input2_unsigned; //OR
+			6 : OUT = input1_signed < input2_signed;  //Compare signed
+			7 : OUT = input1_unsigned < input2_unsigned; //Compare unsigned
+		endcase
+	end
+endmodule
+
+module DataMemory(
+		address,
+		writeData,
+		memWrite,
+		memRead,
+		dataOut,
+		clk
+	);
+	input [31:0] address, writeData;
+	input memWrite,clk;
+	input [1:0] memRead;
+	output reg [31:0] dataOut;
+	reg [7:0] mem [1023:0];
+
+	//TESTING ONLY
+	// initial 
+	// begin 
+	// 	mem[0] = 8'b00100010;
+	// 	mem[1] = 8'b01110010;
+	// 	mem[2] = 8'b11110000;
+	// 	mem[3] = 8'b00000100;
+	// 	mem[4] = 8'b11111010;
+	// 	mem[5] = 8'b00100010;
+	// 	mem[6] = 8'b00111010;
+	// 	mem[7] = 8'b11011010;
+	// end
+	//TESTING ONLY
+
+	always @ (posedge clk)
+	begin
+		if (memWrite)
+		begin
+			mem[address] = writeData[31:24];
+			mem[address+1] = writeData[23:16];
+			mem[address+2] = writeData[15:8];
+			mem[address+3] = writeData[7:0];
+		end
+	end
+	
+	always @(memRead, address)begin
+    if(memRead !== 2'b00)begin
+			case(memRead)
+				//Load word
+				1	:	dataOut = {mem[address],mem[address+1],mem[address+2],mem[address+3]};
+				//Load half word
+				2	:	dataOut = {
+						{16{mem[address][0]}},
+						{mem[address],mem[address+1]}
+					};
+				//Load half word unsigned
+				3	:	dataOut = {
+					{16'b0000000000000000},
+					{mem[address],mem[address+1]}
+				};
+			endcase
+		end
+  end
+	
 endmodule
 
 // module testBench()
